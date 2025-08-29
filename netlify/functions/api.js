9cfe5cbd840bf0281aef7194642d8c939ffeb578
Okata Miarcle <okatamiracle.dev@gmail.com>
2025-08-29 09:37:25 +0100

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 // netlify/functions/api.js
 const serverless = require("serverless-http");
-const app = require("./server.js"); // Updated path to the main server file in the same directory
+const app = require("../../server.js"); // adjust relative path
 
 module.exports.handler = serverless(app);
